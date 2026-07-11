e2f2caa00d03a08ab5063ff9cc3f2f41ef04a3fc
jeyasridhar0371-wq <jeyasridhar0371@gmail.com>
2026-07-11 08:18:13 +0530

    Uncomment DNS server configuration

--- Backend/index.js
+++ Backend/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const mongoose = require('mongoose')
-// const dns = require('dns');
+const dns = require('dns');
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// dns.setServers(['1.1.1.1','8.8.8.8']);
+dns.setServers(['1.1.1.1','8.8.8.8']);
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
